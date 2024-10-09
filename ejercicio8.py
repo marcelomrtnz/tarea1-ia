1375bc48c06b0c3b4139c4120f3fcab37f6d4aea
Marcelo <marcelo.martinez@unah.hn>
2024-10-08 23:12:27 -0600

    ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,12 @@
+def contar_vocales(frase):
+    cantidad_vocales = 0
+    
+    for caracter in frase:
+        if caracter in ["a", "e", "i", "o", "u"]:
+            cantidad_vocales = cantidad_vocales + 1
+
+    return cantidad_vocales
+            
+frase_seleccionada = str(input("Escriba una frase para contar sus vocales: "))
+cantidad_total = contar_vocales(frase_seleccionada)
+print(f"La cantidad total de vocales es: {cantidad_total}")
