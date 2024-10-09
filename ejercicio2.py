79890e14d962784c0e738744e84192d7bae6f355
Marcelo <marcelo.martinez@unah.hn>
2024-10-08 22:08:04 -0600

    Ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,19 @@
+class Libro:
+    titulo = ""
+    autor = ""
+    anio_publicacion = 0
+    numero_paginas = 0
+
+    def __init__(self, titulo, autor, anio_publicacion, numero_paginas) -> None:
+        self.titulo = titulo
+        self.autor = autor
+        self.anio_publicacion = anio_publicacion
+        self.numero_paginas = numero_paginas
+
+
+    def mostrar_informacion(self) -> int:
+        return f"INFORMACIÓN: \nTitulo: {self.titulo} \nAutor: {self.autor} \nAño de publicación: self.anio_publicacion \nNúmero de páginas: {self.numero_paginas}"
+    
+libro = Libro("Blanca Olmedo", "Lucila Gamero de Medina", 1900, 400)
+
+print(libro.mostrar_informacion())
